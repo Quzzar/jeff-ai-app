de96c64727bcaead8c8036056421542ea720563b
Aaron Cassar <aaron@sellscale.com>
2024-02-17 12:38:55 -0800

    init commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex-1`}>
+      <SafeAreaView
+        style={[
+          tw`flex-1 flex-col`,
+          {
+            backgroundColor: '#fff',
+          },
+        ]}
+      >
+        <StatusBar style='auto' />
+        <View style={{ flex: 1 }}>
+          <WebView source={{ uri: 'https://jeff.quzzar.com/' }} style={{ flex: 1 }} />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
